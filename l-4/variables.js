a32298a4de6f96e8f86d9b2b8e5370be1212f9d6
kashifashah1 <kashifa.shah24@gmail.com>
2026-02-04 21:45:46 +0530

    Practised datatypes and variable concepts

--- l-4/variables.js
+++ l-4/variables.js
@@ -1,4 +1,17 @@
-const accountId=14453
-let accountEmail="hitesh@google.com"
-var accountPassword="12345"
-accountCity="Jaipur"
+const accountId = 14453
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId=2 // Not Allowed
+accountEmail = "hc@gmail.com"
+accountPassword = 428357072
+accountCity = "Bangaluru"
+
+console.log(accountId)
+
+/* Prefer not
+to use var because of
+issue in block and functional scope*/
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ l-5/datatypes.js
@@ -0,0 +1,6 @@
+"use strict" // treate all js code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3 + 3);
+console.log("hitesh") // code readablity should be high
