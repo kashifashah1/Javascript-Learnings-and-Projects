756f39e13852856347ccd434b8a9522b3eb2c5da
kashifashah1 <kashifa.shah24@gmail.com>
2026-02-06 18:30:38 +0530

    Gained proper understanding of js dataypes and ECMA standards

--- l-5/datatypes.js
+++ l-5/datatypes.js
@@ -4,3 +4,20 @@
 
 console.log(3 + 3);
 console.log("hitesh") // code readablity should be high
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// nul => standalone value
+// undefined
+//symbol => unique
+//object
+
+console.log(typeof "hitesh")
+console.log(typeof null)
+console.log(typeof undefined)
