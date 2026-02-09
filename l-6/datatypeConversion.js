5414919b218beda762d7267403070174cb4ab705
kashifashah1 <kashifa.shah24@gmail.com>
2026-02-09 19:19:38 +0530

    All datatypes conversion completed

--- l-6/datatypeConversion.js
+++ l-6/datatypeConversion.js
@@ -4,9 +4,25 @@
 let score=false
 // let score=undefined
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn="hitesh"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
