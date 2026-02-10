51dd0a3689539df48874dd13d1d76880d02dfb38
kashifashah1 <kashifa.shah24@gmail.com>
2026-02-10 20:52:46 +0530

    Operations in JS Completed Successfully

--- l-7/operations.js
+++ l-7/operations.js
@@ -9,11 +9,35 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1="hello"
-let str2=" hitesh"
-let str3=str1+str2
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
 // console.log(str3);
 
-console.log("1"+2);
-console.log((1+"2"));
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32   
 
+console.log((3 + 4) * 5 % 3);//2
+
+console.log(true);//true
+console.log(+true);//1
+//+ se conversion hojata h
+// console.log(true+);//error
+
+console.log(+"");//0
+
+let num1, num2, num3
+
+num1 = num2 = mum3 = 2 + 2
+
+let gameCounter=100
+gameCounter++//101-postfix
+++gameCounter//101=prefix
+console.log(gameCounter);//101
+
+
+let a=4;
+console.log(a++);//4
+console.log(++a);//6
