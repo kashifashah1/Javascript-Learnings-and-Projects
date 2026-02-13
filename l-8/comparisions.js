b8f1122700fbdd4a260e5dcb07db2b9098499a6e
kashifashah1 <kashifa.shah24@gmail.com>
2026-02-13 11:51:41 +0530

    Understood comparisions in js

---
+++ l-8/comparisions.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+//while comparing do not compare two different datatypes
+// console.log("2">1);//true
+// console.log("02">1);//true
+
+
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null!=0);//true
+// console.log(null>=0);//true
+
+// console.log(undefined==0);//false
+// console.log(undefined>0);//false
+// console.log(undefined<0);//false
+
+
+console.log("2"===2);//false
